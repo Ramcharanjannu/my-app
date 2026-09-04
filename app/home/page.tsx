89ae0b761cd2f48b578ebde66da280e56478fcb9
Ramcharanjannu <charan192003@gmail.com>
2026-09-04 16:03:16 +0530

    initial commit

---
+++ app/home/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+import { useState } from 'react';
+import Welcome from "../welcome/page";
+import { Apple, Mango, Grapes, Oranges, Banana } from "../welcome/page";
+
+export default function Home() {
+    const [message, setmessage] = useState("")
+    const [count, setcount] = useState(0)
+    function increment() {
+        setcount(count + 1)
+    }
+    function display() {
+        setmessage('RAM')
+    }
+
+    return (
+        <div>
+            <Welcome />
+            <Apple />
+            <Mango name='charan' id={12} />
+            <Grapes name='ramcharan' id={19} />
+            <Oranges onClick={display} />
+            <Banana onClick={increment} />
+
+            <h1>{message}</h1>
+            <h1>{count}</h1>
+
+
+        </div>
+    );
+}
---
+++ app/welcome/page.tsx
@@ -0,0 +1,49 @@
+export default function Welcome() {
+    return (
+        <div>
+            <h1>welcome to home page</h1>
+        </div>
+    )
+}
+
+export function Apple() {
+    return (
+        <div>
+            <button>
+                login
+            </button>
+        </div>
+    )
+}
+
+export function Mango(prop: { name: string, id: number }) {
+    return (
+        <div>
+            <h1> hello {prop.name}
+                ur id is{prop.id}
+            </h1>
+        </div>
+    )
+}
+
+export function Grapes({ name, id }: { name: string, id: number }) {
+    return (
+        <h1> ur name is {name} and ur id is {id}</h1>
+    )
+}
+
+export function Oranges({ onClick }: { onClick: () => void }) {
+    return (
+        <button onClick={onClick}>
+            click here to see ur name
+
+        </button>
+    )
+}
+
+export function Banana({ onClick }: { onClick: () => void }) {
+    return (
+        <button onClick={onClick}>   click here </button>
+    )
+
+}
